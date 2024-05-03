f4d8adb56969659f81de977ebae536ddd99799a3
Zenithao <adrien.le.pober@gmail.com>
2024-05-03 17:43:28 +0200

    create fixtures for vehicles

---
+++ back/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Type;
+use App\Entity\Brand;
+use App\Entity\Color;
+use App\Entity\Model;
+use App\Entity\Motorization;
+use App\Entity\Vehicle;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $brandsData = [
+            'Renault' => 'Megane E-Tech',
+            'Mercedes' => 'Classe S',
+            'Volkswagen' => 'Polo',
+            'Peugeot' => 'DJANGO CLASSIC 50'
+        ];
+
+        foreach ($brandsData as $brandName => $modelName) {
+            $brand = (new Brand())
+                ->setName($brandName);
+            $manager->persist($brand);
+
+            $model = (new Model())
+                ->setName($modelName)
+                ->setBrand($brand);
+            $manager->persist($model);
+
+            $vehicle = (new Vehicle())
+                ->setPower(mt_rand(1, 100))
+                ->setSpace(mt_rand(1, 10))
+                ->setBrand($brand);
+
+            switch($modelName) {
+                case 'Megane E-Tech':
+                    $color = (new Color())->setName('blue');
+                    $manager->persist($color);
+                    $type = (new Type())->setName('Voiture');
+                    $manager->persist($type);
+                    $motorization = (new Motorization())->setName('Electric');
+                    $manager->persist($motorization);
+                    $vehicle
+                        ->addColor($color)
+                        ->setMotorization($motorization)
+                        ->setType($type);
+                    break;
+                case 'Classe S':
+                    $color = (new Color())->setName('black');
+                    $manager->persist($color);
+                    $type = (new Type())->setName('Voiture');
+                    $manager->persist($type);
+                    $motorization = (new Motorization())->setName('Petrol');
+                    $manager->persist($motorization);
+                    $vehicle
+                        ->addColor($color)
+                        ->setMotorization($motorization)
+                        ->setType($type);
+                    break;
+                case 'Polo':
+                    $color = (new Color())->setName('yellow');
+                    $manager->persist($color);
+                    $type = (new Type())->setName('Voiture');
+                    $manager->persist($type);
+                    $motorization = (new Motorization())->setName('Petrol');
+                    $manager->persist($motorization);
+                    $vehicle
+                        ->addColor($color)
+                        ->setMotorization($motorization)
+                        ->setType($type);
+                    break;
+                case 'DJANGO CLASSIC 50':
+                    $color = (new Color())->setName('red');
+                    $manager->persist($color);
+                    $type = (new Type())->setName('Scooter');
+                    $manager->persist($type);
+                    $motorization = (new Motorization())->setName('Petrol');
+                    $manager->persist($motorization);
+                    $vehicle
+                        ->addColor($color)
+                        ->setMotorization($motorization)
+                        ->setType($type);
+                    break;
+                default:
+                    echo 'error';
+            }
+
+            $manager->persist($vehicle);
+        }
+
+        $manager->flush();
+    }
+}
