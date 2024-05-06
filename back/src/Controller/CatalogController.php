15b66bc3e7eb3f9d68be67eaf231c067f08de009
Zenithao <adrien.le.pober@gmail.com>
2024-05-06 17:22:19 +0200

    refactor index in catalogcontroller

--- back/src/Controller/CatalogController.php
+++ back/src/Controller/CatalogController.php
@@ -2,73 +2,16 @@
 
 namespace App\Controller;
 
-use App\AbstractFactory\ElectricVehicleFactory;
-use App\AbstractFactory\PetrolVehicleFactory;
-use App\Repository\VehicleRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Service\VehicleAbstractFactoryService;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CatalogController extends AbstractController
 {
     #[Route('/catalog', name: 'catalog')]
-    public function index(VehicleRepository $vehicleRepository): JsonResponse
+    public function index(VehicleAbstractFactoryService $vehicleAbstractFactoryService): JsonResponse
     {
-        $petrolVehicleFactory = new PetrolVehicleFactory();
-        $electricVehicleFactory = new ElectricVehicleFactory();
-
-        $vehicles = [];
-
-        foreach($vehicleRepository->findElectricCars() as $electricCar) {
-            if(!empty($electricCar)) {
-                $vehicles[] = $electricVehicleFactory->createCar(
-                    $electricCar["id"],
-                    $electricCar["brandName"],
-                    $electricCar["modelName"],
-                    [$electricCar["colorName"]],
-                    $electricCar["power"],
-                    $electricCar["space"]
-                )->getVehicleInfos();
-            }
-        }
-
-        foreach($vehicleRepository->findPetrolCars() as $petrolCar) {
-            if(!empty($petrolCar)) {
-                $vehicles[] = $petrolVehicleFactory->createCar(
-                    $petrolCar["id"],
-                    $petrolCar["brandName"],
-                    $petrolCar["modelName"],
-                    [$petrolCar["colorName"]],
-                    $petrolCar["power"],
-                    $petrolCar["space"]
-                )->getVehicleInfos();
-            }
-        }
-
-        foreach($vehicleRepository->findElectricScooters() as $electricScooter) {
-            if(!empty($eletricScooter)) {
-                $vehicles[] = $electricVehicleFactory->createScooter(
-                    $electricScooter["id"],
-                    $electricScooter["brandName"],
-                    $electricScooter["modelName"],
-                    [$electricScooter["colorName"]],
-                    $electricScooter["power"],
-                )->getVehicleInfos();
-            }
-        }
-
-        foreach($vehicleRepository->findPetrolScooters() as $petrolScooter) {
-            if(!empty($petrolScooter)) {
-                $vehicles[] = $electricVehicleFactory->createScooter(
-                    $petrolScooter["id"],
-                    $petrolScooter["brandName"],
-                    $petrolScooter["modelName"],
-                    [$petrolScooter["colorName"]],
-                    $petrolScooter["power"],
-                )->getVehicleInfos();
-            }
-        }
-
-        return $this->json($vehicles);
+        return $this->json($vehicleAbstractFactoryService->getVehicles());
     }
 }
---
+++ back/src/Service/VehicleAbstractFactoryService.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\VehicleRepository;
+use App\AbstractFactory\PetrolVehicleFactory;
+use App\AbstractFactory\ElectricVehicleFactory;
+
+class VehicleAbstractFactoryService
+{
+    private array $vehicles = [];
+
+    public function __construct(
+        private PetrolVehicleFactory $petrolVehicleFactory,
+        private ElectricVehicleFactory $electricVehicleFactory,
+        private VehicleRepository $vehicleRepository
+    ) { }
+
+    public function getVehicles(): array
+    {
+        $this->makeElectricCars();
+        $this->makePetrolCars();
+        $this->makeElectricScooters();
+        $this->makePetrolScooters();
+        return $this->vehicles;
+    }
+
+    private function makeElectricCars(): void
+    {
+        foreach($this->vehicleRepository->findElectricCars() as $electricCar) {
+            if(!empty($electricCar)) {
+                $this->vehicles[] = $this->electricVehicleFactory->createCar(
+                    $electricCar["id"],
+                    $electricCar["brandName"],
+                    $electricCar["modelName"],
+                    [$electricCar["colorName"]],
+                    $electricCar["power"],
+                    $electricCar["space"]
+                )->getVehicleInfos();
+            }
+        }
+    }
+
+    private function makePetrolCars(): void
+    {
+        foreach($this->vehicleRepository->findPetrolCars() as $petrolCar) {
+            if(!empty($petrolCar)) {
+                $this->vehicles[] = $this->petrolVehicleFactory->createCar(
+                    $petrolCar["id"],
+                    $petrolCar["brandName"],
+                    $petrolCar["modelName"],
+                    [$petrolCar["colorName"]],
+                    $petrolCar["power"],
+                    $petrolCar["space"]
+                )->getVehicleInfos();
+            }
+        }
+    }
+
+    private function makeElectricScooters(): void
+    {
+        foreach($this->vehicleRepository->findElectricScooters() as $electricScooter) {
+            if(!empty($eletricScooter)) {
+                $this->vehicles[] = $this->electricVehicleFactory->createScooter(
+                    $electricScooter["id"],
+                    $electricScooter["brandName"],
+                    $electricScooter["modelName"],
+                    [$electricScooter["colorName"]],
+                    $electricScooter["power"],
+                )->getVehicleInfos();
+            }
+        }
+    }
+
+    private function makePetrolScooters(): void
+    {
+        foreach($this->vehicleRepository->findPetrolScooters() as $petrolScooter) {
+            if(!empty($petrolScooter)) {
+                $this->vehicles[] = $this->electricVehicleFactory->createScooter(
+                    $petrolScooter["id"],
+                    $petrolScooter["brandName"],
+                    $petrolScooter["modelName"],
+                    [$petrolScooter["colorName"]],
+                    $petrolScooter["power"],
+                )->getVehicleInfos();
+            }
+        }
+    }
+}
