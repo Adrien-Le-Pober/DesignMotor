86a05d3620c99663faaf250da02f93d2da2ba502
Zenithao <adrien.le.pober@gmail.com>
2024-05-26 14:36:19 +0200

    add jwt auth to angular

---
+++ front/src/app/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+    private currentUserSubject: BehaviorSubject<any>;
+    public currentUser: Observable<any>;
+
+    constructor(private http: HttpClient) {
+        const currentUser = localStorage.getItem('currentUser');
+        this.currentUserSubject = new BehaviorSubject<any>(currentUser ? JSON.parse(currentUser) : null);
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
+
+    public get currentUserValue() {
+        return this.currentUserSubject.value;
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<any>(`/api/login`, { email, password })
+            .pipe(map(user => {
+                if (user && user.token) {
+                    localStorage.setItem('currentUser', JSON.stringify(user));
+                    this.currentUserSubject.next(user);
+                }
+                return user;
+            }));
+    }
+
+    logout() {
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+    }
+}
