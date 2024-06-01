926abbe103932e07f39b20ca46e6338484758a22
Zenithao <adrien.le.pober@gmail.com>
2024-06-01 13:29:59 +0200

    add bootstrap

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { AuthService } from './auth/auth.service';
 import { UserService } from './user/user.service';
 import { NavbarComponent } from './navbar/navbar.component';
 
@@ -10,23 +9,11 @@ import { NavbarComponent } from './navbar/navbar.component';
   selector: 'app-root',
   standalone: true,
   imports: [
+    RouterModule,
     NavbarComponent
   ],
   templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
-  currentUser: string|null = null;
-
-  constructor(private userService: UserService) { }
-
-  ngOnInit() {
-    this.userService.currentUser$.subscribe((user: string | null) => {
-      this.currentUser = user;
-    });
-  }
-
-  logout() {
-    this.userService.clearCurrentUser();
-  }
 }
--- front/src/app/navbar/navbar.component.ts
+++ front/src/app/navbar/navbar.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { UserService } from '../user/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: 'navbar.component.html',
   styles: ``
 })
 export class NavbarComponent {
+  currentUser: string|null = null;
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.userService.currentUser$.subscribe((user: string | null) => {
+      this.currentUser = user;
+    });
+  }
+
+  logout() {
+    this.userService.clearCurrentUser();
+  }
 }
