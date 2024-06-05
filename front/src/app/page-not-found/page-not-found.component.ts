09786e2213e5e370adfc2280e13427306c37e52f
Zenithao <adrien.le.pober@gmail.com>
2024-06-05 22:53:59 +0200

    add styles on page not found component

--- front/src/app/page-not-found/page-not-found.component.ts
+++ front/src/app/page-not-found/page-not-found.component.ts
@@ -8,9 +8,30 @@ import { RouterModule } from '@angular/router';
     RouterModule
   ],
   template: `
-    <a routerLink="/" routerLinkActive="active-link">Retour à l'accueil</a>
+    <div>
+      <img
+        srcset="../../assets/images/page-not-found/page-not-found-mobile.jpg 500w,
+                ../../assets/images/page-not-found/page-not-found-widescreen.jpg 1064w"
+        sizes="(max-width: 576px) 500px, 1064px"
+        src="../../assets/images/page-not-found/page-not-found-widescreen.jpg"
+        alt="Page introuvable"
+      />
+    </div>
   `,
-  styles: ``
+  styles: `
+    div {
+      display: flex;
+      justify-content: center;
+      background-color: #000;
+      min-height: 100vh;
+    }
+
+    img {
+      width: 100%;
+      max-width: 1064px;
+      object-fit: cover;
+    }
+  `
 })
 export class PageNotFoundComponent {
 
