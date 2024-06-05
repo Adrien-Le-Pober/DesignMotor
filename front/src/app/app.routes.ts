fa53cb1e1e8b388154f9d6173d6ec3855a24d1cc
Zenithao <adrien.le.pober@gmail.com>
2024-06-05 21:24:56 +0200

    remove connection component

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,15 +3,17 @@ import { AdminComponent } from './admin/admin.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { ShowVehicleComponent } from './vehicle/show-vehicle/show-vehicle.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ConnectionComponent } from './connection/connection.component';
 import { AuthGuard } from './auth/auth.guard';
 import { ResendConfirmationEmailComponent } from './user/sign-in/resend-confirmation-email/resend-confirmation-email.component';
 import { ResetPasswordComponent } from './user/reset-password/reset-password.component';
 import { ForgotPasswordComponent } from './user/forgot-password/forgot-password.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
     { path: 'admin', component: AdminComponent },
-    { path: 'connection', component: ConnectionComponent, canActivate: [AuthGuard] },
+    { path: 'connection', component: LoginComponent, canActivate: [AuthGuard] },
+    { path: 'inscription', component: SignInComponent, canActivate: [AuthGuard] },
     { path: 'resend-confirmation-email', component: ResendConfirmationEmailComponent },
     { path: 'reinitialiser-mot-de-passe/:token', component: ResetPasswordComponent },
     { path: 'mot-de-passe-oublie', component: ForgotPasswordComponent },
