d184683e1d6e9114029c9e7f97fcefae7fd07185
Zenithao <adrien.le.pober@gmail.com>
2024-07-01 05:26:10 +0200

    add payment success and payment failure components

--- front/src/app/payment/payment-failure/payment-failure.component.ts
+++ front/src/app/payment/payment-failure/payment-failure.component.ts
@@ -5,11 +5,31 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      payment-failure works!
-    </p>
+    <div class="container d-flex flex-column align-items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-x-circle my-5 text-danger" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+      </svg>
+      <p>Désolé, votre paiement n'a pas pu être validé.</p>
+      <p class="text-center">Réessayez plus tard, si le problème n'est pas résolu, n'hésitez pas à nous contacter.</p>
+      <br>
+      <br>
+      <p>Merci pour votre confiance, à bientôt !</p>
+    </div>
   `,
-  styles: ``
+  styles: `
+    svg {
+      width: 192px;
+      height: 192px;
+    }
+
+    @media screen and (max-width: 776px) {
+      svg {
+        width: 128px;
+        height: 128px;
+      }
+    }
+  `
 })
 export class PaymentFailureComponent {
 
--- front/src/app/payment/payment-success/payment-success.component.ts
+++ front/src/app/payment/payment-success/payment-success.component.ts
@@ -5,12 +5,32 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      payment-success works!
-    </p>
+    <div class="container d-flex flex-column align-items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-check2-circle my-5 text-success" viewBox="0 0 16 16">
+          <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
+          <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+      </svg>
+      <p>Votre paiement à bien été validé.</p>
+      <p class="text-center">Un email vient de vous être envoyé avec la facture.</p>
+      <p class="text-center">Vous pouvez consultez l'historique de vos commandes depuis la rubrique 'mon compte'.</p>
+      <br>
+      <br>
+      <p>Merci pour votre confiance, à bientôt !</p>
+    </div>
   `,
-  styles: ``
+  styles: `
+    svg {
+      width: 256px;
+      height: 256px;
+    }
+
+    @media screen and (max-width: 776px) {
+      svg {
+        width: 128px;
+        height: 128px;
+      }
+    }
+  `
 })
 export class PaymentSuccessComponent {
-
 }
